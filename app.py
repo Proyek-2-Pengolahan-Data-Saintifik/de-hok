dfedbd5c6aceb8007218649790244ef11195c72a
FarraJessica <farrajessica@gmail.com>
2021-06-22 00:50:01 +0700

    Modif Home Page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask, render_template, request, url_for, redirect, render_template
 
 application = Flask(__name__)
 
@@ -6,29 +6,12 @@ application = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@application.route('/blog', methods=['GET', 'POST'])
-def blog():
-    if request.method == 'POST' :
-        return redirect(url_for('index'))
-    return render_template('blog.html')
-
 @application.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST' :
         return redirect(url_for('index'))
+    
     return render_template('contact.html')
-
-@application.route('/blog-detail', methods=['GET', 'POST'])
-def blog_detail():
-    if request.method == 'POST' :
-        return redirect(url_for('index'))
-    return render_template('blog-detail.html')
-
-@application.route('/project-detail', methods=['GET', 'POST'])
-def project_detail():
-    if request.method == 'POST' :
-        return redirect(url_for('index'))
-    return render_template('project-detail.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
