dabb4cc337f53e2950dd63798970d58f549978d8
Linda Damayanti <linda.damayanti.tif419@polban.ac.id>
2021-06-30 20:42:20 +0700

    hosting heroku

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request, url_for, redirect, render_template
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/aboutUs', methods=['GET', 'POST'])
+@app.route('/aboutUs', methods=['GET', 'POST'])
 def aboutUs():
     if request.method == 'POST' :
         return redirect(url_for('index'))
     return render_template('aboutUs.html')
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
